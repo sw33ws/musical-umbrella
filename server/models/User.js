6108021b6facd9999b00e753e0c0389b50fbf5a2
Seb <gillisl22@outlook.com>
2022-07-27 11:36:23 -0400

    adding a user model, and schema

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 5,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+})
+
+// Set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // Compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    await bcrypt.compare(password, this.password);
+  };  
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ server/schema/resolvers.js
@@ -0,0 +1,5 @@
+const { AuthenticationError } = require('apollo-server-express');
+// const { Model Imports } = require('../models');
+// const { Auth if needed } = require('../utils/auth');
+
+module.exports = resolvers;
