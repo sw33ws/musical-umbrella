1bfc68dc5822e92cd095cbd7a34b36572e6cbf8b
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-30 22:36:08 -0400

    newbranch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+<<<<<<< HEAD
 // import Contact from "client/src/component/Contact/Contact.js";
 // import Footer from "client/src/component/Footer/Footer.js";
 import './App.css';
+=======
+import Contact from "client/src/component/Contact/Contact.js";
+import Footer from "client/src/component/Footer/Footer.js";
+import "./App.css";
+import { useState } from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+>>>>>>> 9126bf294ab4feaa64a4b2c74b343bcaac39df72
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  const location = useLocation();
+
   return (
+<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
@@ -19,6 +35,25 @@ function App() {
 Git Commit: A Meeting of Charity.</a>
       </header>
     </div>
+=======
+    <>
+      <CssBaseline />
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/signup" element={<Signup setAuth={setAuth} />} />
+        <Route
+          path="/"
+          element={
+            auth ? (
+              <Home setAuth={setAuth} />
+            ) : (
+              <Navigate to="/login" state={{ from: location }} replace />
+            )
+          }
+        />
+      </Routes>
+    </>
+>>>>>>> 9126bf294ab4feaa64a4b2c74b343bcaac39df72
   );
 }
 
