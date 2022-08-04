855e8d6f8ad05c92a2d3997ad727e7789f3618da
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-08-03 20:58:10 -0400

    fixes

---
+++ client/src/Pages/Commits.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Commits() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        paddingTop: "300px",
+      }}
+    >
+      <h1>Commits</h1>
+    </div>
+  );
+}
+
+export default Commits;
